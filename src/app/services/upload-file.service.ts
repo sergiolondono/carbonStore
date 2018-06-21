b85dab38603db88dbf7c1104deae75c2444f637b
sergiolondono <sergio.londonog@gmail.com>
2018-06-21 15:09:26 -0500

    20180621. 1510

--- src/app/services/upload-file.service.ts
+++ src/app/services/upload-file.service.ts
@@ -1,49 +1,52 @@
 import {Injectable} from '@angular/core';
-import {AngularFireDatabase} from 'angularfire2/database';
+import {AngularFireDatabase, snapshotChanges} from 'angularfire2/database';
 import * as firebase from 'firebase';
 
 import {FileUpload} from '../fileupload';
 
 @Injectable()
 export class UploadFileService {
- 
+
   constructor(private db: AngularFireDatabase) {}
- 
+
   private basePath = '/uploads';
 
+  manageFileToStorage(fileUpload: FileUpload, progress: {percentage: number}){
+    let response = this.pushFileToStorage(fileUpload, progress);
+    console.log(response);
+    this.saveFileData(response);
+  }
+
   pushFileToStorage(fileUpload: FileUpload, progress: {percentage: number}) {
+
     const storageRef = firebase.storage().ref();
     const uploadTask = storageRef.child(`${this.basePath}/${fileUpload.file.name}`).put(fileUpload.file);
-    let urlFile = "";
-
+    
     uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
       (snapshot) => {
         // in progress
         const snap = snapshot as firebase.storage.UploadTaskSnapshot
-        progress.percentage = Math.round((snap.bytesTransferred / snap.totalBytes) * 100);
+        progress.percentage = Math.round((snap.bytesTransferred / snap.totalBytes) * 100);        
       },
       (error) => {
         // fail
         console.log(error);
       },
       () => {
-
-        // storageRef.child(`${this.basePath}/${fileUpload.file.name}`).getDownloadURL()        
-        // .then(function(url) {   
-        //   urlFile = url; 
-        // }); 
-
-        //console.log(uploadTask.snapshot);
-        //fileUpload.name = fileUpload.file.name
-
-
-         fileUpload.name = fileUpload.file.name;
-         this.saveFileData(fileUpload);
+         storageRef.child(`${this.basePath}/${fileUpload.file.name}`).getDownloadURL()
+        .then((url) => {
+          fileUpload.name = fileUpload.file.name;
+          fileUpload.url = url;
+          this.saveFileData(fileUpload);
+        });
       }
     );
+    return fileUpload;
   }
- 
+
   private saveFileData(fileUpload: FileUpload) {
+    console.log("saveFileData Reached");
+    console.log(fileUpload);
     this.db.list(`${this.basePath}/`).push(fileUpload);
   }
 }
